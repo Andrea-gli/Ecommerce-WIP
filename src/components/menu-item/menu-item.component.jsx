98259479ce78d62cc7536d6bed895db921e9f506
AndreaLi <andrea.li8341@hotmail.com>
2021-03-29 13:58:51 -0700

    style menu-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,12 +4,13 @@ import '../menu-item/menu-item.component.scss';
 export default function MenuItem({ title, imageUrl, size }) {
   console.log(imageUrl);
   return (
-    <div
-      className='menu-item'
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
       <div className='content'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>SHOP NOW</span>
