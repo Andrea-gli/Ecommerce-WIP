bf17a1ca3fb151032d17d3223c8d93f2491eff4b
AndreaLi <andrea.li8341@hotmail.com>
2021-04-06 10:59:45 -0700

    add signin page, components, and style

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/header/header.component';
 import ShopPage from './pages/shop/shop.component';
 import ContactPage from './pages/contact/contactpage.component';
+import signInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
         <Route path='/contact' component={ContactPage} />
+        <Route path='/signin' component={signInAndSignUpPage} />
       </Switch>
     </div>
   );
